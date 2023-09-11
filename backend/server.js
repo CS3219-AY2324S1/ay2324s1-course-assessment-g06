a0233d3f09348f2ad538c5e2eeccf20e89902c2c
Evan <crossjab@gmail.com>
2023-09-11 22:33:48 +0800

    frontend-backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.listen(3000, () => {
+  console.log('Server has started! Open http://localhost:3000');
+});
+
+app.get('', async (req, res) => {
+  res.send('Hello World!');
+});
