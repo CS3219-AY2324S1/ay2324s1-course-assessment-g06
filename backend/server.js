a62b9c923ef26249d2ba64893b883c46dae137d8
Jun Kang <junkang@Juns-MacBook-Air.local>
2023-09-14 23:50:47 +0800

    Add env support and backend structure

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
+// Use env file
+require('dotenv').config(); 
 const express = require('express');
-
 const app = express();
 app.listen(3000, () => {
   console.log('Server has started! Open http://localhost:3000');
