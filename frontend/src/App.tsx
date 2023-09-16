a9b3fdf04843877f4693577571e0cbb6d434dc53
Kalaris Ng <kalaris3255@gmail.com>
2023-09-16 14:52:18 +0800

    link row to question

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./Table/Table";
 import BasicTable from "./Table/Table";
+import Question from "./Question/Question";
 
 function App() {
   return (
     <div className="App">
-      <BasicTable />
+      <Routes>
+        <Route path="/" element={<BasicTable />} />
+        <Route path="/questions/:frontendQuestionId" element={<Question />} />
+      </Routes>
     </div>
   );
 }
