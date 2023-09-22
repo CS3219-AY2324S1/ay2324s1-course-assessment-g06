5e3c686c8f2a83f3034b6a968c141101e10a2c82
Isabel Chong <IsabelChong.CYJI@gmail.com>
2023-09-22 12:51:47 +0800

    Update fluid styling

--- frontend/src/Question/Question.tsx
+++ frontend/src/Question/Question.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./Question.css";
 import { styled } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import { Button, Container, Grid, Paper } from "@mui/material";
 
 const DeleteButton = styled(Button)`
   background-color: #ff5733; /* Change the background color */
@@ -24,6 +24,24 @@ interface Question {
   category: string;
   topics: string;
 }
+
+const QuestionWrapper = styled(Container)(({ theme }) => ({
+  backgroundColor: 'rgb(231, 231, 231)',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  fontWeight: 'bold',
+  textAlign: 'center',
+  borderRadius: '50px',
+}));
+
+const CategoryWrapper = styled(Container)(({ theme }) => ({
+  backgroundColor: 'rgb(255, 192, 203)',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  fontWeight: 'bold',
+  textAlign: 'center',
+  borderRadius: '50px',
+}));
 
 export default function Question() {
   const { id } = useParams<{ id: string }>();
@@ -53,6 +71,13 @@ export default function Question() {
   useEffect(() => {
     fetchDataWithDelay();
   }, [id]);
+
+
+  function wrapPreTags(content : string) {
+    // Use regular expressions to add a class to <pre> tags
+    const wrappedContent = content.replace(/<pre>/g, '<pre class="pre-wrap">');
+    return wrappedContent;
+  }
 
   const handleDelete = () => {
     // Send a DELETE request to delete the question
@@ -89,47 +114,55 @@ export default function Question() {
 
   // Check if question is defined before accessing its properties
   if (question !== undefined) {
-    console.log(question.title);
     return (
-      <div>
-        <div className="box">
-          <div className="rectangle">
-            <div className="question-wrapper">
+      <Container maxWidth="lg" style={{ margin: "0 auto" }}> 
+      {/* <div className="box"> */}
+
+      <Grid item xs={12}>
+          <Button variant="contained" onClick={handleBack}>
+            Back </Button>
+        </Grid>
+        
+        <Grid sx={{ flexGrow: 1 }} container spacing={1}>
+
+          <Grid item xs={12}>
               <h1>{question.title}</h1>
-            </div>
-            <div className="category-group-wrapper">
-              <div className="category-group">
-                <p>{question.category}</p>
+          </Grid>
+
+          <Grid item xs={6}>
+            <QuestionWrapper>{question.category}</QuestionWrapper>
+          </Grid>
+
+          <Grid item xs={6}>
+            <CategoryWrapper>{question.difficulty}</CategoryWrapper>
+          </Grid>
+
+          <Container maxWidth="lg">
+            <Paper
+            style={{
+              padding: "20px",
+              margin: "10px",
+            }}>
+            <Grid item xs={12}>
+              <div className="content-wrapper">
+                <div dangerouslySetInnerHTML={{ __html: wrapPreTags(question.content) }} />
               </div>
-            </div>
-            <div className="difficulty-group-wrapper">
-              <div className="difficulty-group">
-                <p>{question.difficulty}</p>
-              </div>
-            </div>
-            <div className="content-group-wrapper">
-              <div className="content-group">
-                <div dangerouslySetInnerHTML={{ __html: question.content }} />
-              </div>
-            </div>
-            <div className="button-container">
+            </Grid>
+            </Paper>
+          </Container>
+
+          <Grid item xs={6}>
               <DeleteButton variant="contained" onClick={handleDelete}>
                 Delete
               </DeleteButton>
-            </div>
-            <div className="button-container">
+          </Grid>
+          <Grid item xs={6}>
               <Button variant="contained" onClick={handleUpdate}>
                 Update
               </Button>
-            </div>
-            <div className="button-container">
-              <Button variant="contained" onClick={handleBack}>
-                Back
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
+          </Grid>
+        </Grid>
+      </Container>
     );
   } else {
     return <div>Question not found.</div>;
