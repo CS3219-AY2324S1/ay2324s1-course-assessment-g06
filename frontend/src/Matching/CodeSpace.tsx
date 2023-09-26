9172a638f27e8b65a47dac47d08d3d11711b1511
Jun Kang <irminrics@gmail.com>
2023-09-26 23:41:38 +0800

    Add collaborative code mirror

--- frontend/src/Matching/CodeSpace.tsx
+++ frontend/src/Matching/CodeSpace.tsx
@@ -1,7 +1,8 @@
-// CodeSpace.js
 import React, { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
-import { io, Socket } from 'socket.io-client'; // Import Socket type from 'socket.io-client'
+import { io, Socket } from 'socket.io-client';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 
 interface Question {
   _id: string;
@@ -19,27 +20,50 @@ const CodeSpace = () => {
   const { socketId, difficulty } = location.state || {};
   const [socket, setSocket] = useState<Socket | null>(null);
   const [question, setQuestion] = useState<Question | null>(null);
+  const [code, setCode] = useState("console.log('hello world!');");
+  const [value, setValue] = React.useState("console.log('hello world!');");
+
+  const onChange = React.useCallback((val: string, viewUpdate: any) => {
+    console.log('val:', val);
+    setValue(val);
+  
+    // Emit the 'codeChange' event to the server only if it's a change by this client
+    if (socket) {
+      socket.emit('codeChange', val, roomId); // Pass roomId or any identifier
+      console.log('emiting codechange from client');
+    }
+  }, [socket]);
 
   const fetchData = () => {
     fetch(`http://localhost:3002/api/room/${roomId}`)
-    .then((response) => response.json())
-    .then((data) => {
-      setQuestion(data)
-      console.log(data)
-    })
-    .catch((error) => {
-      console.log(error)
-      setQuestion(null)
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestion(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setQuestion(null);
+      });
   };
 
   useEffect(() => {
+    // Create a socket connection
     const matchedSocket = io('http://localhost:3002', {
       query: { roomId },
     });
 
     matchedSocket.on('connect', () => {
       console.log('Connected to matched socket');
+      console.log(matchedSocket);
+
+      // Emit the "joinRoom" event when the socket connects
+      matchedSocket.emit('joinRoom', roomId);
+    });
+
+    // Listen for 'codeChange' events from the server
+    matchedSocket.on('codeChange', (newCode: string) => {
+      setValue(newCode); // Update the value with the new code
     });
 
     setSocket(matchedSocket);
@@ -50,56 +74,37 @@ const CodeSpace = () => {
     };
   }, [roomId]);
 
+  return (
+    <div>
+      <h2>Welcome, {socketId || 'Loading...'}</h2>
+      <p>
+        You are matched with another user using difficulty: {difficulty || 'Not selected'}
+      </p>
 
-  if (question !== null) {
-    return (
-      <div>
-        <h2>Welcome, {socketId || 'Loading...'}</h2>
-        <p>
-          You are matched with another user using difficulty: {difficulty || 'Not selected'}
-        </p>
+      <br />
+      <br />
 
-        <br />
-        <br />
-
-      
+      {question !== null ? (
         <div>
-          <div className="box">
-            <div className="rectangle">
-              <div className="question-wrapper">
-                <h1>{question.title}</h1>
-              </div>
-              <div className="category-group-wrapper">
-                <div className="category-group">
-                  <p>{question.category}</p>
-                </div>
-              </div>
-              <div className="difficulty-group-wrapper">
-                <div className="difficulty-group">
-                  <p>{question.difficulty}</p>
-                </div>
-              </div>
-              <div className="content-group-wrapper">
-                <div className="content-group">
-                  <div dangerouslySetInnerHTML={{ __html: question.content }} />
-                </div>
-              </div>
-
-            </div>
+          <div>
+            <h1>{question.title}</h1>
+            <p>Category: {question.category}</p>
+            <p>Difficulty: {question.difficulty}</p>
+            <div dangerouslySetInnerHTML={{ __html: question.content }} />
           </div>
         </div>
-      </div>
-    )
-  } else {
-    return (
+      ) : (
+        <p>Loading question...</p>
+      )}
+
+      <br />
+      <br />
+
       <div>
-        <h2>Welcome, {socketId || 'Loading...'}</h2>
-        <p>
-          You are matched with another user using difficulty: {difficulty || 'Not selected'}
-        </p>
+        <CodeMirror value={value} height="200px" onChange={onChange} />
       </div>
-    )
-  }
+    </div>
+  );
 };
 
 export default CodeSpace;
