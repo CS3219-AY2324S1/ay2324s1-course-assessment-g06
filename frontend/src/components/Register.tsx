1edc37c49875b42f943e9bd57ca44bb23e8a7fe4
Jun Kang <irminrics@gmail.com>
2023-10-10 23:51:32 +0800

    Revamp sign up page

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-
 import IUser from "../types/user.type";
 import { register } from "../services/auth.service";
+import mascot from '../images/mascot.png';
+import { Link } from "react-router-dom";
 
 const Register: React.FC = () => {
   const [successful, setSuccessful] = useState<boolean>(false);
@@ -63,76 +64,164 @@ const Register: React.FC = () => {
     );
   };
 
+  const mascotStyles = {
+    height: 'auto',
+    maxWidth: '100%',
+    width: 'auto',
+  };
+
   return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleRegister}
-        >
-          <Form>
-            {!successful && (
-              <div>
-                <div className="form-group">
-                  <label htmlFor="username"> Username </label>
-                  <Field name="username" type="text" className="form-control" />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className="alert alert-danger"
-                  />
-                </div>
+    <div className="container">
+      <div className="row align-items-center justify-content-center">
+        <div className="col-md-5">
+          <div className="col-md-10 mx-auto">
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleRegister}
+            >
+              {({ handleSubmit }) => (
+                <Form onSubmit={handleSubmit}>
+                  {!successful && (
+                    <div>
+                      <div className="form-floating mb-3">
+                        <Field
+                          name="username"
+                          type="text"
+                          className="form-control input-lg"
+                          id="floatingUsername"
+                          placeholder=" "
+                          style={{ borderRadius: "15px" }}
+                        />
+                        <label
+                          htmlFor="floatingUsername"
+                          style={{
+                            paddingLeft: "10px",
+                            paddingTop: 0,
+                            paddingBottom: 0,
+                            paddingRight: 0,
+                            lineHeight: "3rem",
+                            margin: 0,
+                          }}
+                        >
+                          Username
+                        </label>
+                        <ErrorMessage
+                          name="username"
+                          component="div"
+                          className="alert alert-danger"
+                        />
+                      </div>
 
-                <div className="form-group">
-                  <label htmlFor="email"> Email </label>
-                  <Field name="email" type="email" className="form-control" />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="alert alert-danger"
-                  />
-                </div>
+                      <div className="form-floating mb-3">
+                        <Field
+                          name="email"
+                          type="email"
+                          className="form-control input-lg"
+                          id="floatingEmail"
+                          placeholder=" "
+                          style={{ borderRadius: "15px" }}
+                        />
+                        <label
+                          htmlFor="floatingEmail"
+                          style={{
+                            paddingLeft: "10px",
+                            paddingTop: 0,
+                            paddingBottom: 0,
+                            paddingRight: 0,
+                            lineHeight: "3rem",
+                            margin: 0,
+                          }}
+                        >
+                          Email
+                        </label>
+                        <ErrorMessage
+                          name="email"
+                          component="div"
+                          className="alert alert-danger"
+                        />
+                      </div>
 
-                <div className="form-group">
-                  <label htmlFor="password"> Password </label>
-                  <Field
-                    name="password"
-                    type="password"
-                    className="form-control"
-                  />
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="alert alert-danger"
-                  />
-                </div>
+                      <div className="form-floating">
+                        <Field
+                          name="password"
+                          type="password"
+                          className="form-control input-lg"
+                          id="floatingPassword"
+                          placeholder=" "
+                          style={{ borderRadius: "15px" }}
+                        />
+                        <label
+                          htmlFor="floatingPassword"
+                          style={{
+                            paddingLeft: "10px",
+                            paddingTop: 0,
+                            paddingBottom: 0,
+                            paddingRight: 0,
+                            margin: 0,
+                            lineHeight: "3rem",
+                          }}
+                        >
+                          Password
+                        </label>
+                        <ErrorMessage
+                          name="password"
+                          component="div"
+                          className="alert alert-danger"
+                        />
+                      </div>
 
-                <div className="form-group">
-                  <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                </div>
-              </div>
-            )}
+                      <div className="form-group mt-3 d-flex align-items-center">
+                        <div className="col-md-5" style={{ fontFamily: 'Inter, sans-serif', fontWeight: 'bold', color: 'rgba(0, 0, 0, 0.5)'  }}>
+                          Account exists?
+                          <br/>
+                          <Link to="/login" style={{ textDecoration: 'underline', color: 'rgba(0, 0, 0, 0.5)' }}>
+                            Login
+                          </Link>
+                        </div>
+                          <div className="col-md-2" />
+                          <div className="col-md-5 text-center">
+                            <button
+                              type="submit"
+                              className="btn btn-block rounded-pill"
+                              style={{
+                                backgroundColor: '#6C63FF',
+                                color: 'white',
+                                fontFamily: 'Inter, sans-serif',
+                                fontWeight: 'bold',
+                                height: '50px',
+                              }}
+                            >
+                              Sign Up
+                            </button>
+                          </div>
+                        </div>
+                    </div>
+                  )}
 
-            {message && (
-              <div className="form-group">
-                <div
-                  className={
-                    successful ? "alert alert-success" : "alert alert-danger"
-                  }
-                  role="alert"
-                >
-                  {message}
-                </div>
-              </div>
-            )}
-          </Form>
-        </Formik>
+                  {message && (
+                    <div className="form-group">
+                      <div
+                        className={
+                          successful
+                            ? "alert alert-success"
+                            : "alert alert-danger"
+                        }
+                        role="alert"
+                      >
+                        {message}
+                      </div>
+                    </div>
+                  )}
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+
+        <div className="col-md-7 d-flex align-items-center justify-content-center">
+          <img src={mascot} alt="mascot" style={mascotStyles} />
+        </div>
       </div>
     </div>
   );
