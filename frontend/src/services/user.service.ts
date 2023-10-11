ff40bceaacfb10f8b96c0c415bcf612e415123df
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-10-11 11:12:13 +0800

    Move API URL to env file

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -3,7 +3,7 @@ import authHeader from './auth-header';
 
 // const API_URL = 'http://localhost:3003/api/test/';
 
-const API_URL = "http://localhost:3003/api/auth/";
+const API_URL = process.env.USER_API_URL;
 
 export const getPublicContent = () => {
   return axios.get(API_URL + "all");
