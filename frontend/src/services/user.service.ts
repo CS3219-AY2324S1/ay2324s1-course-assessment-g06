6a6d7f1cb45b2c6fc4eeb30b6478b88b0c911d93
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-10-16 20:06:54 +0800

    Alter auth API to get from env

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-// const API_URL = 'http://localhost:3003/api/test/';
-
-// const API_URL = process.env.USER_API_URL;
-
-const API_URL = "http://localhost:3003/api/auth/";
-
+const API_URL = process.env.REACT_APP_USER_SVC_URL;
+// const API_URL = "http://localhost:3003/api/auth/";
 
 export const getPublicContent = () => {
   return axios.get(API_URL + "all");
