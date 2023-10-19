e9f5f972b34100c2b19d3ec68eb66dc1284aca8f
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-10-19 17:05:17 +0800

    Remove logging comments

--- question-service/middleware/verifyUserAdmin.js
+++ question-service/middleware/verifyUserAdmin.js
@@ -3,13 +3,10 @@ const axios = require('axios');
 const userSvcApi = "http://localhost:3003/api/auth";
 
 const verifyUserAdmin = (req, res, next) => {
-    console.log('Verifying user admin token...'); // Debugging message to indicate the start of the function
 
     const token = req.headers['x-access-token'];
-    console.log('Token:', token); // Display the token for debugging purposes
 
     if (!token) {
-        console.log('No token provided!'); // Debugging message
         return res.status(403).send({ message: "No token provided!" });
     }
 
@@ -18,16 +15,13 @@ const verifyUserAdmin = (req, res, next) => {
         headers: { 'x-access-token': token }
     })
     .then(adminResponse => {
-        console.log('Admin verification response:', adminResponse.status, adminResponse.data); // Display the response status and data
         if (adminResponse.status === 200) {
             next();  // Move to the next middleware or request handler
         } else {
-            console.log('User is not an admin:', adminResponse.status); // Debugging message
             res.status(401).send({ message: "User is not an admin!" });
         }
     })
     .catch(error => {
-        console.error('Error during admin check:', error); // Display the error for debugging purposes
         res.status(401).send({ message: "Unauthorized! by qns svc" });
     });
 };
--- question-service/middleware/verifyUserToken.js
+++ question-service/middleware/verifyUserToken.js
@@ -3,13 +3,10 @@ const axios = require('axios');
 const userSvcApi = "http://localhost:3003/api/auth";
 
 const verifyUserToken = (req, res, next) => {
-    console.log('Verifying user token...'); // Debugging message to indicate the start of the function
 
     const token = req.headers['x-access-token'];
-    console.log('Token:', token); // Display the token for debugging purposes
 
     if (!token) {
-        console.log('No token provided!'); // Debugging message
         return res.status(403).send({ message: "No token provided!" });
     }
 
@@ -17,17 +14,13 @@ const verifyUserToken = (req, res, next) => {
         headers: { 'x-access-token': token }
     })
     .then(response => {
-        console.log('Response from userSvcApi:', response.status, response.data); // Display the response status and data
         if (response.status === 200) {
             next();
         } else {
-            // If the status code is not 200 or any other unexpected status code is received
-            console.log('Unexpected response status:', response.status); // Debugging message
             res.status(401).send({ message: "Unauthorized by qnssvc!" });
         }
     })
     .catch(error => {
-        console.error('Error during token verification:', error); // Display the error for debugging purposes
         res.status(401).send({ message: "Unauthorized! by qns svc" });
     });
 };
