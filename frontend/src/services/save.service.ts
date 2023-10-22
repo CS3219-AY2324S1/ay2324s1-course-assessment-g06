e6a99127d8f75fe3bad17c1aad0fc03ba7047412
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-10-22 09:33:55 +0800

    Debug save

--- frontend/src/services/save.service.ts
+++ frontend/src/services/save.service.ts
@@ -4,10 +4,13 @@ import authHeader from "./auth-header";
 const API_URL = "http://localhost:3002/api/save/";
 
 export const savesession = (questionId: string, difficulty: string, code: string) => {
+
+  const header = authHeader();
+  console.log(header);
   return axios.post(API_URL + "savesession", {
     questionId,
     difficulty,
     code,
   }, 
-  { headers: authHeader() });
+  { headers: header });
 };
