141dccd748350b7d5df28b72512772b538ae8802
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-11-03 00:15:37 +0800

    Add Admin Protected route to addquestions

---
+++ frontend/src/components/Unauthorised/Unauthorised.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Unauthorised: React.FC = () => {
+  return (
+    <div>
+      <h1>Unauthorised!</h1>
+      <p>Sorry, you are not authorised to view this page.</p>
+    </div>
+  );
+};
+
+export default Unauthorised;
