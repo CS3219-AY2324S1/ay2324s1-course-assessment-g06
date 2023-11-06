6f54d81cca7e43f53ea5f13f6f481cd62f183dcc
Jun Kang <irminrics@gmail.com>
2023-11-06 10:26:31 +0800

    Fix create question validation for soft delete

--- question-service/controllers/questionController.js
+++ question-service/controllers/questionController.js
@@ -65,8 +65,8 @@ module.exports = {
     const { title, frontendQuestionId, difficulty, content, category, topics } =
       req.body;
 
-    // Check if a question with the same title already exists
-    Question.findOne({ title })
+    // Check if a question with the same title already exists and not soft deleted
+    Question.findOne({ title, isDeleted: false })
       .then((existingQuestion) => {
         if (existingQuestion) {
           return res
