80a96dac5e816ae9891df6d098674e1447179805
Jun Kang <irminrics@gmail.com>
2023-11-06 10:39:30 +0800

    Fix update question for soft delete

--- question-service/controllers/questionController.js
+++ question-service/controllers/questionController.js
@@ -107,8 +107,8 @@ module.exports = {
     const { title, frontendQuestionId, difficulty, content, category, topics } =
       req.body;
 
-    // Check if a question with the same title other than itself already exists
-    Question.findOne({ title })
+    // Check if a question with the same title and not soft deleted other than itself already exists
+    Question.findOne({ title, isDeleted: false })
       .then((existingQuestion) => {
         if (existingQuestion && existingQuestion.id != id) {
           return res
