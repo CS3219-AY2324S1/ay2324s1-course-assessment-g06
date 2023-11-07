df68feb644da2a518e173618e0b5f21c138f5c05
Sean Wong <58366602+ExtraShotLatte@users.noreply.github.com>
2023-11-07 21:13:37 +0800

    Add comments

--- user-service/config/auth.config.js
+++ user-service/config/auth.config.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv').config({ path: '../.env' });
 
-
 // This takes the JWT_SECRET from the .env file and exports it to be used in other files
 module.exports = {
   secret: process.env.JWT_SECRET
