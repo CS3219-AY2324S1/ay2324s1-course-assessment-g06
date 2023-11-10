038c8be258135a02247c631d972aa2a0cd17d3af
Jun Kang <irminrics@gmail.com>
2023-11-10 23:29:25 +0800

    Complete assignment 2

--- user-service/middleware/authJwt.js
+++ user-service/middleware/authJwt.js
@@ -30,10 +30,10 @@ isAdmin = (req, res, next) => {
   User.findByPk(req.userId).then(user => {
     user.getRoles().then(roles => {
       for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "admin") {
+
           next();
           return;
-        }
+        
       }
 
       res.status(403).send({
